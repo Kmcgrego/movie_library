96a44beedaee803e40f76517f5ccb8e105c64f90
Kmcgrego <Kmcgrego@users.noreply.github.com>
2023-05-19 08:44:14 -0400

    Added api structure json

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//custom styles
+import './css/app.scss'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
