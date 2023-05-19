0198606f6e325544c7d71706daaac107705c43b4
Kmcgrego <Kmcgrego@users.noreply.github.com>
2023-05-19 08:40:52 -0400

    Add two Panel

---
+++ src/components/LeftPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-navigation-drawer class="navigation" theme="dark" permanent>
+    <v-list color="transparent">
+      <v-list-item title="Search"></v-list-item>
+      <v-list-item title="History"></v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn block>
+          Logout
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
