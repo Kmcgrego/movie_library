3dc9a5463b1e37a12aadc02e730742d74423ba5b
Kmcgrego <Kmcgrego@users.noreply.github.com>
2023-05-19 08:42:59 -0400

    Add filterheader

---
+++ src/components/FilterHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>Test</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
